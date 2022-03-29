2b65457b64d8fd40a523bb8cbef193cf60d954ed
Guillem Orellana Trullols <guillem.orellana@gmail.com>
2022-03-29 15:39:01 +0200

    Update __main__.py

--- madgan/__main__.py
+++ madgan/__main__.py
@@ -2,7 +2,12 @@ import typer
 from madgan.train import train
 
 _madgan_cli = typer.Typer(name="MAD-GAN CLI")
-_madgan_cli.command(train)
+_madgan_cli.command(name="train")(train)
+
+
+@_madgan_cli.callback()
+def main() -> None:
+    """MAD-GAN Command Line Interface."""
 
 if __name__ == "__main__":
     _madgan_cli()
